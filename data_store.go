2bfeab3b070b92041cd8acedabe1dc5d1ee038fa
hkozacz <hubert.kozacz@openx.com>
2024-03-27 23:41:37 +0100

    Working ds <3

--- data_store.go
+++ data_store.go
@@ -1,20 +1,67 @@
 package main
 
-type IDataStore[T any] interface {
-	Load(key T)
-	LoadMany()
-}
-type DataStore[T any] struct {
-	IDataStore[T]
-	batchLoadFunc func(param T)
+import (
+	"sync"
+	"time"
+)
+
+type IDataStore[T string | int, R any] interface {
+	Load(key T, result *chan R, wg *sync.WaitGroup)
+	batchLoad()
 }
 
-func NewDataStore[T any](batchLoadFun func(param T)) *DataStore[T] {
-	ds := DataStore[T]{}
-	ds.batchLoadFunc = batchLoadFun
+type DataStore[T string | int, R any] struct {
+	IDataStore[T, R]
+	batchLoadFunc  func(key []T) []R
+	isStartedToken chan int
+	wg             sync.WaitGroup
+	channels       map[T]*chan R
+}
+
+func (ds *DataStore[T, R]) batchLoad() {
+	defer ds.wg.Done()
+	mapLen := len(ds.channels)
+	for {
+		time.Sleep(100)
+		if mapLen == len(ds.channels) {
+			ds.isStartedToken = make(chan int, 1)
+			ds.isStartedToken <- 1
+			break
+		} else {
+			mapLen = len(ds.channels)
+		}
+	}
+
+	var results []T
+	for key := range ds.channels {
+		results = append(results, key)
+	}
+	batched := ds.batchLoadFunc(results)
+	index := 0
+	for _, value := range ds.channels {
+		*value <- batched[index]
+		index++
+	}
+
+}
+
+func (ds *DataStore[T, R]) Load(key T, result *chan R, wg *sync.WaitGroup) {
+	defer wg.Done()
+	_, ok := <-ds.isStartedToken
+	if ok {
+		close(ds.isStartedToken)
+		ds.wg.Add(1)
+		go ds.batchLoad()
+	}
+	ds.channels[key] = result
+	ds.wg.Wait()
+}
+
+func NewDataStore[T string | int, R any](batchLoadFunc func(key []T) []R) *DataStore[T, R] {
+	ds := DataStore[T, R]{}
+	ds.batchLoadFunc = batchLoadFunc
+	ds.isStartedToken = make(chan int, 1)
+	ds.isStartedToken <- 1
+	ds.channels = make(map[T]*chan R)
 	return &ds
-}
-
-func (ds *DataStore[T]) Load(key T) {
-	ds.batchLoadFunc(key)
 }
--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-func batchLoad(foo string) {
-	fmt.Println(foo)
+func batchLoadFunc(keys []string) []int {
+	var res []int
+	fmt.Println("once?")
+	for _, element := range keys {
+		res = append(res, len(element))
+	}
+	return res
 }
 
 func main() {
-	ds := NewDataStore(batchLoad)
-	ds.Load("Hello world")
+	ds := NewDataStore[string, int](batchLoadFunc)
+	var wg sync.WaitGroup
+	result := make(chan int, 100)
+	result2 := make(chan int, 100)
+	wg.Add(1)
+	go ds.Load("key", &result, &wg)
+	wg.Add(1)
+	go ds.Load("key2", &result2, &wg)
+	wg.Wait()
+	fmt.Println(<-result)
+	fmt.Println(<-result2)
 }
